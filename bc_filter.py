7385eb5fcfdb2016d4049fdf6c60ad39befdb646
Xianan Liu <xl586@nyu.edu>
2018-01-24 23:12:23 -0500

    rewrite the yield items in the generate functions

--- bc_filter.py
+++ bc_filter.py
@@ -1,3 +1,26 @@
+# generate all possible combinations for a string with given number of mismatchs
+def mismatch_re(s, mis):
+    import itertools
+    import re
+    all_list = []
+    mismatch_pos = itertools.combinations(range(len(s)), mis)
+    for i in mismatch_pos:
+        temp_s = s
+        for pos in i:
+            temp_s = "".join((temp_s[:pos], ".", temp_s[pos+1:]))
+        all_list.append(temp_s)
+    return '|'.join(all_list)
+
+# get full list of multitag used in one run and join them together to generate one big regex to filter multitag
+def get_full_re(multitag):
+    import re
+    multitag_list = []
+    for i in multitag:
+        multitag_list.append(mismatch_re(i, 2))
+    return re.compile('|'.join(multitag_list))
+
+
+
 # Follow biopython FastqGeneralIterator generator function
 def barcode_filter_generator(handle, read_direction):
     import re
@@ -5,12 +28,21 @@ def barcode_filter_generator(handle, read_direction):
     lintag1_re = re.compile('\D*?(.ACC|T.CC|TA.C|TAC.)\D{4,7}?AA\D{4,7}?TT\D{4,7}?TT\D{4,7}?(.TAA|A.AA|AT.A|ATA.)\D*')
     lintag2_re = re.compile('\D*?(.ACC|T.CC|TA.C|TAC.)\D{4,7}?AA\D{4,7}?AA\D{4,7}?TT\D{4,7}?(.TAC|T.AC|TT.C|TTA.)\D*')
     bc_len = 38
+    multitag_pos = 8
+    multitag = ['GCTTGACTTCTC','CAAGAAAGGGTT', 'ATGCGAGGCGTCC', 'CATAGGGTATTG', 'TTTTTGGCGCGC', 'TGGAAGGCACTC', 'CCGGGCTGTTTG', 'CTACTCCCTGCA', 'AGTGATGATCTG', 'TTCGGTGCTTAA']
+    
+    # forward multitag big regex
+    f_multitag = [i[0:6] for i in multitag]
+    # reverse multitag big regex
+    r_multitag = [i[6:] for i in multitag]
     if read_direction == "f":
         bc_filter = lintag1_re
         bc_pos = 57
+        multitag_filter = get_full_re(f_multitag)
     elif read_direction == "r":
         bc_filter = lintag2_re
         bc_pos = 43
+        multitag_filter = get_full_re(r_multitag)
     count = 0
     handle_readline = handle.readline
     while True:
@@ -58,39 +90,51 @@ def barcode_filter_generator(handle, read_direction):
                                 " for %s (%i and %i)."
                                 % (title_line, seq_len, len(quality_string)))
         bc_grep = bc_filter.match(seq_string[bc_pos:bc_pos+bc_len])
-
-        if bc_grep != None:
+        multitag_grep = multitag_filter.match(seq_string[multitag_pos:])
+        if bc_grep != None and multitag_grep != None:
             quality_score = np.fromstring(quality_string[bc_grep.start() + bc_pos: bc_grep.end() + bc_pos], np.int8)-33
-            if np.mean(quality_score) >= 30:
+            if np.mean(quality_score) >= 29:
 #                count += 1
-                yield (title_line, seq_string, quality_string, count)
+                yield (multitag_grep.group(), bc_grep.group(), seq_string[0:8], count)
             else:
                 continue
         else:
             continue
     print (count)
     raise StopIteration
+
+
+
 # Build two separate dictionaries with line number as key and quality matching read as value
 # What if the fastq file is very big, not sure the size limit for dictionary
 # I need to optimize the dictionary, empty the dictionaries maybe every 100000 reads
 bc1_dict = {}
 bc2_dict = {}
-for t, s, q, c in barcode_filter_generator(open("small_f.fastq", "r"), "f"):
-    bc1_dict[c] = s
-for t, s, q, c in barcode_filter_generator(open("small_r.fastq", "r"), "r"):
-    bc2_dict[c] = s
-# find common key and sorted
-dbc_keys = sorted(list(bc1_dict.keys() & bc2_dict.keys()))
 # open files to write quality matching reads
 vars()["f_bc.txt"] = open("f_bc.txt", "w")
 vars()["r_bc.txt"] = open("r_bc.txt", "w")
+vars()["f_multitag"] = open("f_multitag.txt", "w")
+vars()["r_multitag"] = open("r_multitag.txt", "w")
+vars()["f_umi"] = open("f_umi.txt", "w")
+vars()["r_umi"] = open("r_umi.txt", "w")
+for (m1, bc1, umi1, c1), (m2, bc2, umi2, c2) in zip(barcode_filter_generator(open("small_f.fastq", "r"), "f"), 
+        barcode_filter_generator(open("small_r.fastq", "r"), "r")):
+    bc1_dict[c1], bc2_dict[c2] = [m1, bc1, umi1], [m2, bc2, umi2] 
+
+# find common key and sorted
+dbc_keys = sorted(list(bc1_dict.keys() & bc2_dict.keys()))
+
 # write reads into files in sorted order
 for i in dbc_keys:
-    vars()["f_bc.txt"].write(bc1_dict[i] + '\n')
-    vars()["r_bc.txt"].write(bc2_dict[i] + '\n')
+    vars()["f_bc.txt"].write(bc1_dict[i][1] + '\n')
+    vars()["r_bc.txt"].write(bc2_dict[i][1] + '\n')
+    vars()["f_multitag"].write(bc1_dict[i][0] + '\n')
+    vars()["r_multitag"].write(bc2_dict[i][0] + '\n')
+    vars()["f_umi"].write(bc1_dict[i][2] + '\n')
+    vars()["r_umi"].write(bc2_dict[i][2] + '\n')
 vars()["f_bc.txt"].close()
 vars()["r_bc.txt"].close()
-#bc1 = barcode_filter_generator(open("small_f.fastq", "r"), "f")
-#bc2 = barcode_filter_generator(open("small_r.fastq", "r"), "r")
-#for (t1, s1, q1), (t2, s2, q2) in zip(bc1, bc2):
-
+vars()["f_multitag"].close()
+vars()["r_multitag"].close()
+vars()["f_umi"].close()
+vars()["r_umi"].close()
